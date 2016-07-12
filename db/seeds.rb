312da54b95b22ff3858159970d43bb8b4aa4171c
Francois Belanger <frank@smartbuildingapps.com>
2016-07-12 16:15:56 -0400

    setting up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'pp'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,5 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+
+pp CreateAdminService.new.call
+
+case Rails.env
+when 'development'
+  100.times { |index| pp User.create_with(password: "password").find_or_create_by(email: "user#{index}@mail.com") }
+end
